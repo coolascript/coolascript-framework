66698eededed8db85f7e4d821dba4606440f437c
Vitaliy <coolascript@gmail.com>
2016-02-15 21:12:26 +0200

    Fields: Multiselect, Radio, Checkbox
    Updated field classes

--- csframework/field/checkbox.php
+++ csframework/field/checkbox.php
@@ -1,30 +1,35 @@
 <?php
-/**
-* Checkbox field
-*/
-
 namespace csframework;
+/**
+* Checkbox form field
+*/
 class FieldCheckbox extends Field
 {
-	
-	function __construct($args)
+	/**
+	 * Instantiate a class object
+	 * @param csframework\Csframework $app  App instance
+	 * @param array $args Field parameters
+	 */
+	function __construct( $app, $args )
 	{
 		$this->setSanitize( 'bool' );
-		parent::__construct($args);
+		parent::__construct( $app, $args );
 	}
 
+	/**
+	 * Render a field HTML
+	 * @return void
+	 */
 	public function render()
 	{
 		?>
-		<div class="field field-checkbox<?php echo esc_attr( $this->_depend ? ' depend-field' : '' );  ?>"<?php echo wp_kses_post( $this->_depend ? ' data-depend="' . implode( ';', $this->getDependecies() ) . '"' : '' );  ?>>
-			<label for="<?php echo esc_attr( $this->getInputId() ); ?>" class="checkbox-label">
-				<input type="checkbox" name="<?php echo esc_attr( Csframework::getFieldsVar() . $this->getInputName() ); ?>" id="<?php echo esc_attr( $this->getInputId() ); ?>" value="1" <?php checked( (bool) !is_null( $this->_value ) ? $this->_value : (bool) $this->_default, true ); ?> class="<?php echo esc_attr( $this->_class ); ?>" />
-				<?php echo wp_kses_post( $this->_label ); ?>
+		<div class="csframework-field csframework-field-checkbox<?php echo esc_attr( $this->_depend ? ' depend-field' : '' );  ?>"<?php echo ( bool ) $this->_depend ? ' data-depend="' . esc_attr( implode( ';', $this->getDependecies() ) ) . '"' : '';  ?>>
+			<label for="<?php echo esc_attr( $this->getInputId() ); ?>" class="csframework-checkbox-label">
+				<input type="checkbox" name="<?php echo esc_attr( $this->_app->getFieldsVar() . $this->getInputName() ); ?>" id="<?php echo esc_attr( $this->getInputId() ); ?>" value="1" <?php checked( ( bool ) !is_null( $this->_value ) ? $this->_value : ( bool ) $this->_default, true ); ?> class="<?php echo esc_attr( $this->_class ); ?>" />
+				<?php echo apply_filters( 'the_title', $this->_label ); ?>
 			</label>
 			<?php if ( $this->_description ): ?>
-				<div class="field-description">
-					<?php echo wp_kses_post( $this->_description ); ?>
-				</div>
+				<?php echo apply_filters( 'the_content', $this->_description ); ?>
 			<?php endif ?>
 		</div>
 		<?php
--- csframework/field/radio.php
+++ csframework/field/radio.php
@@ -1,46 +1,64 @@
 <?php
+namespace csframework;
 /**
 * Radio buttons field
 */
-
-namespace csframework;
 class FieldRadio extends Field
 {
+	/**
+	 * Radio buttons
+	 * @var array Key based array ( value => Label )
+	 */
 	protected $_values = array();
 	
-	function __construct($args)
+	/**
+	 * Instantiate a class object
+	 * @param csframework\Csframework $app  App instance
+	 * @param array $args Field parameters
+	 */
+	function __construct( $app, $args )
 	{
-		parent::__construct($args);
+		parent::__construct( $app, $args );
 	}
 
-	public function setValues($val)
+	/**
+	 * Set radio buttons
+	 * @param array $val Field options. Key based array ( value => Label )
+	 */
+	public function setValues( $val )
 	{
 		$this->_values = is_array( $val ) ? $val : array();
 		return $this;
 	}
 
+	/**
+	 * Retriev Field options
+	 * @return array Key based array ( value => Label )
+	 */
 	public function getValues()
 	{
 		return $this->_values;
 	}
 
+	/**
+	 * Render a field HTML
+	 * @return void
+	 */
 	public function render()
 	{
 		?>
-		<div class="field field-radio<?php echo esc_attr( $this->_depend ? ' depend-field' : '' );  ?>"<?php echo wp_kses_post( $this->_depend ? ' data-depend="' . implode( ';', $this->getDependecies() ) . '"' : '' );  ?>>
-			<?php if ($this->_label && $this->_show_label): ?>
-				<h5 class="label"><?php echo wp_kses_post( $this->_label ); ?>:</h5>
+		<div class="csframework-field csframework-field-radio<?php echo esc_attr( $this->_depend ? ' depend-field' : '' );  ?>"<?php echo ( bool ) $this->_depend ? ' data-depend="' . esc_attr( implode( ';', $this->getDependecies() ) ) . '"' : '';  ?>>
+			<?php if ( $this->_label && $this->_show_label ): ?>
+				<h5 class="csframework-label"><?php echo apply_filters( 'the_title', $this->_label ); ?>:</h5>
 			<?php endif ?>
-			<?php $i=0; foreach ($this->_values as $value => $label): ?>
-				<label for="<?php echo esc_attr( $this->getInputId() . '-' . $i ); ?>" class="radio-label <?php echo esc_attr( $this->_class ); ?>">
-					<input type="radio" name="<?php echo esc_attr( Csframework::getFieldsVar() . $this->getInputName() ); ?>" id="<?php echo esc_attr( $this->getInputId() . '-' . $i ); ?>" value="<?php echo esc_attr( $value ); ?>" <?php checked( $value, !is_null( $this->_value ) && !empty( $this->_value ) ? $this->_value : $this->_default ); ?>>
-					<?php echo wp_kses_post( $label ); ?>
+			<?php $i=0; foreach ( $this->_values as $value => $label ): ?>
+				<label for="<?php echo esc_attr( $this->getInputId() . '-' . $i ); ?>" class="csframework-radio-label <?php echo esc_attr( $this->_class ); ?>">
+					<input type="radio" name="<?php echo esc_attr( $this->_app->getFieldsVar() . $this->getInputName() ); ?>" id="<?php echo esc_attr( $this->getInputId() . '-' . $i ); ?>" value="<?php echo esc_attr( $value ); ?>" <?php checked( $value, !is_null( $this->_value ) && !empty( $this->_value ) ? $this->_value : $this->_default ); ?>>
+					<?php echo apply_filters( 'the_title', $label ); ?>
 				</label>
 			<?php $i++; endforeach ?>
 			<?php if ( $this->_description ): ?>
-				<div class="field-description">
-					<?php echo wp_kses_post( $this->_description ); ?>
-				</div>
+				<?php echo apply_filters( 'the_content', $this->_description ); ?>
 			<?php endif ?>
 		</div>
 		<?php
