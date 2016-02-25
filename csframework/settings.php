b499b24af3173a9235caecd74393f6d02f94a18b
Vitaliy <coolascript@gmail.com>
2016-02-25 17:43:46 +0200

    Settings page
    Updated class which provide an ability to create settings page

--- csframework/settings.php
+++ csframework/settings.php
@@ -1,39 +1,48 @@
 <?php
-/**
-* 
-*/
-
 namespace csframework;
-class Settings extends Abstractive
+/**
+* App settings page
+*/
+class Settings extends Base
 {
 	protected $_name = 'settings';
+	protected $_title = '';
+	protected $_capability = 'activate_plugins';
+	protected $_parent = null;
+	protected $_icon = '';
+	protected $_position = null;
 	protected $_sections = array();
 	protected $_options = array();
-	protected $_theme = null;
+	protected $_app = null;
 
-	function __construct() {
-		$this->_theme = Csframework::getInstance();
-	}
-
-	public function isActive()
-	{
-		return ( bool ) sizeof( $this->_options );
+	function __construct( $app, $title, $name, $capability = 'activate_plugins', $parent = null, $icon_url = '', $position = null ) {
+		$this->_app = $app;
+		$this->_name = sanitize_title( $name );
+		$this->_title = apply_filters( 'the_title', $title );
+		$this->_capability = ( string ) $capability;
+		$this->_parent = $parent;
+		$this->_icon = ( string ) $icon_url;
+		$this->_position = $position;
 	}
 
 	public function addSettingsPage()
 	{
-		add_theme_page( __( 'Theme Options', Csframework::getTextDomain() ), __( 'Theme Options', Csframework::getTextDomain() ), 'edit_theme_options', 'theme-options', array( $this, 'showSettings' ) );
+		if ( !is_null( $this->_parent ) ) {
+			add_submenu_page ( $this->_parent, $this->_title, $this->_title, $this->_capability, $this->_name, array( $this, 'showSettings' ) );
+		} else {
+			add_menu_page ( $this->_title, $this->_title, $this->_capability, $this->_name, array( $this, 'showSettings' ), $this->_icon, $this->_position );
+		}
 	}
 
 	public function init()
 	{
-		register_setting( 'theme_options', Csframework::getFieldsVar(), array( $this, 'sanitize' ) );
+		register_setting( $this->_name, $this->_app->getFieldsVar(), array( $this, 'sanitize' ) );
 		
-		foreach ($this->_sections as $slug => $section) {
-			add_settings_section( $slug, $section['name'], array( $this, 'renderSection' ), 'theme-options' );
+		foreach ( $this->_sections as $slug => $section ) {
+			add_settings_section( $slug, $section['name'], array( $this, 'renderSection' ), $this->_name );
 			
-			foreach ($section['fields'] as $name) {
-				add_settings_field( 'option_' . $name, $this->_options[$name]->getLabel(), array( $this->_options[$name]->setValue( $this->getOptionValue( $name ) ), 'render' ), 'theme-options', $slug );
+			foreach ( $section['fields'] as $name ) {
+				add_settings_field( 'option_' . $name, $this->_options[$name]->getLabel(), array( $this->_options[$name]->setValue( $this->getOptionValue( $name ) ), 'render' ), $this->_name, $slug );
 			}
 		}
 
@@ -41,9 +50,7 @@ class Settings extends Abstractive
 
 	public function sanitize( $val )
 	{
-		update_option( 'csframework', $val[$this->_name] );
-		/*foreach ($val[$this->_name] as $key => $value) {
-		}*/
+		update_option( $this->_name, $val[$this->_name] );
 	}
 
 	public function addSection( $slug, $name )
@@ -66,13 +73,13 @@ class Settings extends Abstractive
 	{
 		if ( is_array( $field_options ) ) {
 			if ( isset( $this->_sections[$section] ) && !isset( $this->_options[$field_options['name']] ) ) {
-				$field_class = 'rhtheme\Field' . ucfirst( $field_options['type'] );
+				$field_class = 'csframework\Field' . ucfirst( $field_options['type'] );
 				if ( class_exists( $field_class ) ) {
 					$field_options['parent'] = $this;
 					$this->_sections[$section]['fields'][] = $field_options['name'];
-					$this->_options[$field_options['name']] = new $field_class( $field_options );
+					$this->_options[$field_options['name']] = new $field_class( $this->_app, $field_options );
 				} else {
-					throw new \Exception( sprintf( __( "Unknown field type `%s`", Csframework::getTextDomain() ), $type ) );
+					throw new \Exception( sprintf( __( "csframework\Settings: Unknown field type `%s`", 'coolascript-framework' ), $type ) );
 				}
 			}
 		} elseif ( is_object( $field_options ) ) {
@@ -93,7 +100,7 @@ class Settings extends Abstractive
 	public function getOptionValue( $name )
 	{
 		if ( isset( $this->_options[$name] ) ) {
-			if ( $options = get_option( 'csframework', false ) ) {
+			if ( $options = get_option( $this->_name, false ) ) {
 				return isset( $options[$name] ) ? $options[$name] : $this->_options[$name]->getDefault();
 			} elseif ( $val = get_option( $name, $this->_options[$name]->getDefault() ) ) {
 				return $val;
@@ -107,27 +114,21 @@ class Settings extends Abstractive
 		return $this->_name;
 	}
 
-	private function _addAssets()
+	public function addAdminAssets()
 	{
-		$this->_theme->scripts->addScript( 'theme-accordion-init', array(
-			'url' => get_template_directory_uri() . '/assets/csframework/js/accordion-init.js',
-			'deps' => array( 'jquery', 'jquery-ui-accordion' ),
-			'ver' => '1.0.0',
-			'load' => true,
-			'load_check' => 'is_admin',
-		) );
+		wp_enqueue_script( 'csframework-accordion' );
 	}
 
 	public function renderSection() {}
 	protected function _renderSection( $slug ) {
 		?>
-			<h3 class="settings-section-title section-<?php echo esc_attr( $slug ); ?>">
-				<?php echo wp_kses_post( $this->_sections[$slug]['name'] ); ?>
+			<h3 class="csframewoork-settings-section-title csframewoork-section-<?php echo esc_attr( $slug ); ?>">
+				<?php echo apply_filters( 'the_title', $this->_sections[$slug]['name'] ); ?>
 			</h3>
-			<div class="settings-section-fields section-<?php echo esc_attr( $slug ); ?>">
-				<ul class="settings-fields">
+			<div class="csframewoork-settings-section-fields csframewoork-section-<?php echo esc_attr( $slug ); ?>">
+				<ul class="csframewoork-settings-fields">
 				<?php foreach ($this->_sections[$slug]['fields'] as $field): ?>
-					<li class="settings-field">
+					<li class="csframewoork-settings-field">
 						<?php if ( $this->_options[$field]->getSanitize() == 'google_font' ): ?>
 							<?php $values = $this->_options[$field]->getValues() ?>
 							<?php $value = explode( ':', $this->getOptionValue( $field ) ) ?>
@@ -148,16 +149,16 @@ class Settings extends Abstractive
 		?>
 		<div class="wrap">
 			<form action="<?php echo admin_url( 'options.php' ); ?>" method="post" enctype="multipart/form-data">
-				<?php settings_fields( 'theme_options' ); ?>
+				<?php settings_fields( $this->_name ); ?>
 				<h2>
 					<?php echo get_admin_page_title(); ?>
 				</h2>
-				<div class="theme-accordion">
+				<div class="csframewoork-accordion">
 					<?php foreach ($this->_sections as $slug => $section): ?>
 						<?php $this->_renderSection( $slug ) ?>
 					<?php endforeach ?>
 				</div>
-				<input name="Submit" type="submit" class="button button-primary" value="<?php _e( 'Save Changes', Csframework::getTextDomain() ); ?>" />
+				<input name="Submit" type="submit" class="button button-primary" value="<?php _e( 'Save Changes', 'coolascript-framework' ); ?>" />
 			</form>
 		</div>
 		<?php
@@ -165,7 +166,6 @@ class Settings extends Abstractive
 
 	public function render()
 	{
-		$this->_addAssets();
 		add_action( 'admin_menu', array( $this, 'addSettingsPage' ) );
 		add_action( 'admin_init', array( $this, 'init' ) );
 	}
