404d6d37fe4c1f12b20546bae77583e33dade543
Vitaliy <coolascript@gmail.com>
2016-05-05 18:04:35 +0300

    Theme and Plugin updaters
    Added update checkers:
    1. Theme checker from
    http://w-shadow.com/blog/2011/06/02/automatic-updates-for-commercial-themes/
    2. Plugin checker from
    http://w-shadow.com/blog/2010/09/02/automatic-updates-for-any-plugin/

---
+++ csframework/update/plugin.php
@@ -0,0 +1,7 @@
+<?php
+/**
+* Plugin updater
+*/
+namespace csframework;
+require CSFRAMEWORK_PLUGIN_DIR . 'csframework/lib/plugin-updates/plugin-update-checker.php';
+class UpdatePlugin extends \PucFactory {}
